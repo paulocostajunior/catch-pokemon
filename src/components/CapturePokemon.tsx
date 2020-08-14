558612cec1bbfd8a57f40f2bf70089e7e3059fc7
paulocostajunior <paulo.costa@mag-it.com.br>
2020-08-13 22:56:53 -0300

    First Commit

---
+++ src/components/CapturePokemon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = { }
+
+const CapturePokemon: React.FC<Props> = () => (
+  <div>
+    Capture Pokemon
+  </div>
+);
+
+export default CapturePokemon;
---
+++ src/components/MyPokemon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = { }
+
+const MyPokemon: React.FC<Props> = () => (
+  <div>
+    <h2>Pikachu</h2>
+    <h2>Bulbasaur</h2>
+    <h2>Chikorita</h2>
+  </div>
+);
+
+export default MyPokemon;
